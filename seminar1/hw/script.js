4ba48fa03a1508c3c518fad2c8f58d3cb9ba81c8
Anna Sisenina <yuta4ka71@gmail.com>
2024-04-15 11:03:27 +0300

    1st seminar and he are added

---
+++ seminar1/hw/script.js
@@ -0,0 +1,32 @@
+const http = require('http');
+let count = 0;
+
+const server = http.createServer((req, res) => {
+    console.log('Запрос получен');
+    
+
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=UTF-8'
+        });
+        count++;
+        res.end(`<a href="http://localhost:3001/about">About</a> <p>Посещений: ${count}</p>`)
+    } else if (req.url === '/about') {
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=UTF-8'
+        });
+        count++;
+        res.end(`<a href="http://localhost:3001/">Main</a> <p>Посещений: ${count}</p>`)
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html; charset=UTF-8'
+        });
+        res.end('<h1> Page not founded </h1>')
+    }
+});
+
+const port = 3001;
+
+server.listen(port, () => {
+    console.log(`Сервер запущен на порту ${port}`);
+});
